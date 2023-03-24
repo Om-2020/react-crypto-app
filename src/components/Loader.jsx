b606e70d88ff21cf07d3be6aa6a3a7ac0bdcfb98
Om-2020 <omprakashbharti1123@gmail.com>
2023-03-24 19:49:17 +0530

    first change

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import { Spinner, VStack,Box } from '@chakra-ui/react';
+import React from 'react';
+
+function Loader() {
+  return (
+    <VStack h="90vh" justifyContent={"center"}>
+
+        <Box transform={"scale(3)"}>
+            <Spinner size={"xl"}></Spinner>
+
+        </Box>
+
+    </VStack>
+  )
+}
+
+export default Loader;
